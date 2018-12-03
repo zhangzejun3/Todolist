baf236f98e1cd5bc8087247ae0a2dd16c4c73487
zhangzejun3 <zzj2472@163.com>
2018-12-03 16:58:20 +0800

    first commit

---
+++ build/precache-manifest.affa33eddbd3b4ca025336625aa0698a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e562c4f199b59cc5277",
+    "url": "/Todolist/build/static/js/runtime~main.9e562c4f.js"
+  },
+  {
+    "revision": "b4df3a3d0a4938ca6b14",
+    "url": "/Todolist/build/static/js/main.b4df3a3d.chunk.js"
+  },
+  {
+    "revision": "c2ea6c283125f00601cc",
+    "url": "/Todolist/build/static/js/1.c2ea6c28.chunk.js"
+  },
+  {
+    "revision": "b4df3a3d0a4938ca6b14",
+    "url": "/Todolist/build/static/css/main.d373a702.chunk.css"
+  },
+  {
+    "revision": "945d45cf7ffb53efa5fd75de9f372ed4",
+    "url": "/Todolist/build/index.html"
+  }
+];
