9079b674775aa0ef714d115d7ac0cdc92d863765
zhangzejun3 <zzj2472@163.com>
2018-12-04 19:03:49 +0800

    TodoItem组件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
     constructor() {
@@ -9,13 +10,14 @@ class App extends Component {
             newTodo: 'test',
             todoList: [
                 { id: 1, title: '第1个待办' },
-                { id: 2, title: '第2个待办' }
+                { id: 2, title: '第2个待办' },
+                { id: 3, title: '第3个待办' }
             ]
         }
     }
     render() {
         let todos = this.state.todoList.map((item, index) => {
-            return <li>{item.title}</li>
+            return <TodoItem todo={item}/>
         })
         return (
             <div className='App'>
